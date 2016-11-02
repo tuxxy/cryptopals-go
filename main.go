e39d38407b9373eea68826bfa059c38bc01e1515
John Pacific <john.pacific@eventboard.io>
2016-11-02 17:12:23 -0600

    Complete Set 1 Challenge 5

--- main.go
+++ main.go
@@ -25,4 +25,9 @@ func main() {
     fmt.Println("-- Challenge 4 --")
     plaintext, plain_val = Basics_Chall4()
     fmt.Printf("Plaintext: %v\nValue: %v\n", plaintext, plain_val)
+
+    // Set 1 Challenge 5
+    fmt.Println("-- Challenge 5 --")
+    ciphertext := Basics_Chall5()
+    fmt.Printf("Ciphertext: %v\n", ciphertext)
 }
