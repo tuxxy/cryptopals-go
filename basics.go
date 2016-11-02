2280998655ea5462e8b55a39c80546eea1176e59
John Pacific <john.pacific@eventboard.io>
2016-11-02 11:42:40 -0600

    Add basics code..

---
+++ basics.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "encoding/hex"
+    "encoding/base64"
+)
+
+func Bytify(s string) *[]byte {
+    byte_msg, err := hex.DecodeString(s)
+    if err != nil {
+        panic(err)
+    }
+    return &byte_msg
+}
+
+func Stringify(src []byte) string {
+    return hex.EncodeToString(src)
+}
+
+func Bytes_b64(src *[]byte) string {
+    str_msg := base64.StdEncoding.EncodeToString(*src)
+    return str_msg
+}
+
+func Basics_Chall1() string {
+    hex_string := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    b64_msg := Bytes_b64(Bytify(hex_string))
+    return b64_msg
+}
+
+func Basics_Chall2() 
