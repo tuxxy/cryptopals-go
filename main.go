ef18658dd8b2f5a75bc1e32f0e12773789e78d24
John Pacific <john.pacific@eventboard.io>
2016-11-02 11:38:38 -0600

    Perform Set 1, Challenge 1

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Starting Set 1!")
+    // Set 1 Challenge 1
+    fmt.Println("-- Challenge 1 --")
+    basics_str := Basics_Chall1()
+    fmt.Println(basics_str + "\n")
+}
