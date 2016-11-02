365466c3e7ac8447f5f5b5a525ddd598dd4ed2a6
John Pacific <john.pacific@eventboard.io>
2016-11-02 11:51:15 -0600

    Add XOR_Bytes function

--- basics.go
+++ basics.go
@@ -22,6 +22,12 @@ func Bytes_b64(src *[]byte) string {
     return str_msg
 }
 
+func XOR_Bytes(src_a, src_b, dest []byte) {
+    for i := 0; i < len(src_a); ++i {
+        dest[i] = src_a[i] ^ src_b[i]
+    }
+}
+
 func Basics_Chall1() string {
     hex_string := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     b64_msg := Bytes_b64(Bytify(hex_string))
